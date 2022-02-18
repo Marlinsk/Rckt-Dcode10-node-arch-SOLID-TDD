4f3321b9bfe437ea2940daf956aef29a6517029e
Marlon <Marlonnlmx@outlook.com>
2022-02-18 08:11:21 -0300

    Update to Entity

--- src/core/domain/Entity.ts
+++ src/core/domain/Entity.ts
@@ -4,6 +4,10 @@ export abstract class Entity<T> {
 
     private _id: string;
     public props: T;
+
+    get id() { 
+        return this._id; 
+    }
     
     constructor(props: T, id?: string) {
         this.props = props;
